f449eda0bc731456187f7f5ef9f6e53f1061d480
hussainkor <hussainkor@gmail.com>
2024-03-21 11:27:25 +0530

    chnages done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
+
+const message = [
+  'Learn React *',
+  'Apply for Jobs#',
+  'Yay! I got a job',
+  'Invest your new Income!',
+  'Yes started the Investing journey'
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <div>
+    <Steps />
+  </div>
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (step <= message.length - 1) {
+      setStep((s) => s + 1);
+    }
+  }
+
+  return <>
+    <button onClick={() => setIsOpen((is) => !is)} className='open-close-btn'>{isOpen ? 'Close' : 'Open'}</button>
+    {isOpen && <div className='steps'>
+      <div className='numbers'>
+        {message.map((name, i) => {
+          return <div className={step >= i + 1 ? 'active' : ''} key={i}>{i + 1}</div>
+        })}
+      </div>
+      <p className='message'><span className='stepper'>Step {step}:</span> {message[step - 1]}</p>
+
+      <div className='buttons'>
+        {step > 1 && <button onClick={handlePrevious}>Previous</button>}
+        {step < message.length && <button onClick={handleNext}>Next</button>}
+      </div>
     </div>
-  );
+    }</>
 }
 
 export default App;
